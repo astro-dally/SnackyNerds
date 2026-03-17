0396135cdc29164b460d135602299a8ca28f4feb
astro_dally <animemedia64@gmail.com>
2026-03-17 20:55:20 +0530

    fixed the errors

--- server/src/index.js
+++ server/src/index.js
@@ -4,5 +4,6 @@ const app = require('./app');
 const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => {
+    // eslint-disable-next-line no-console
     console.log(`Server running on port ${PORT}`);
 });
